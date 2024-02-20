9ba49330c132ee84ebd679b160b36a03dce04558
Robert Onder <134645209+Rob0717@users.noreply.github.com>
2024-02-20 14:40:26 +0100

    commit

--- web/Views/RegistrationTemplate.tpl.php
+++ web/Views/RegistrationTemplate.tpl.php
@@ -51,87 +51,91 @@ global $tplData;
 <?php
 if(!$tplData['jePrihlasen']){
 ?>
-<div class="container" id="registracniFormular">
-    <div class="row justify-content-center">
-        <div class="col-lg-6">
-            <h2 class="text-center">Registrační formulář</h2>
-            <form method="POST">
-                <div class="row">
-                    <div class="col mt-2">
-                        <label for="name">Jméno</label>
-                        <input type="text" class="form-control" name="name" id="name" placeholder="Zadejte jméno">
+    <div class="flex justify-center items-center h-screen font-roboto">
+        <form class="grid space-x-5 justify-center items-center bg-emerald-100 rounded-2xl space-y-3 p-5" method="POST">
+            <div class="col-span-2">
+                <h2 class="text-center">Registrační formulář</h2>
+            </div>
+            <div class="grid-cols-1">
+                    <div class="flex space-x-2">
+                        <div class="grid">
+                            <label for="name">Jméno</label>
+                            <input type="text" name="name" id="name" placeholder="Zadejte jméno">
+                        </div>
+                        <div class="grid">
+                            <label for="surname">Příjmení</label>
+                            <input type="text" name="surname" id="surname" placeholder="Zadejte příjmení">
+                        </div>
                     </div>
-                    <div class="col mt-2">
-                        <label for="surname">Příjmení</label>
-                        <input type="text" class="form-control" name="surname" id="surname" placeholder="Zadejte příjmení">
+                    <div class="grid">
+                        <label for="email">Emailová adresa</label>
+                        <input type="email" name="email" id="email" placeholder="Zadejte email">
                     </div>
-                </div>
-                <div class="col mt-2">
-                    <label for="email">Emailová adresa</label>
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Zadejte email">
-                </div>
-                <div class="row">
-                    <div class="col mt-2">
-                        <label for="password1">Heslo</label>
-                        <input type="password" class="form-control" name="password1" id="password1" placeholder="Zadejte heslo">
-                    </div>
-                    <div class="col mt-2">
-                        <label for="password2">Heslo znovu</label>
-                        <input type="password" class="form-control" name="password2" id="password2" placeholder="Zadejte heslo znovu">
-                    </div>
-                </div>
-                <h4 class="text-center col mt-4">Bydliště</h4>
-                <div class="row">
-                    <div class="col mt-2">
-                        <label for="street">Ulice</label>
-                        <input type="text" class="form-control" name="street" id="street" placeholder="ulice">
-                    </div>
-                    <div class="col mt-2">
-                        <label for="housenumber">ČP</label>
-                        <input type="number" min="1" max="1000" class="form-control" name="housenumber" id="housenumber" placeholder="čp">
-                    </div>
-                    <div class="col mt-2">
-                        <label for="psc">PSČ</label>
-                        <input type="number" min="10000" max="79999" class="form-control" name="psc" id="psc" placeholder="psč">
+                    <div>
+                        <div class="grid">
+                            <label for="password1">Heslo</label>
+                            <input type="password" name="password1" id="password1" placeholder="Zadejte heslo">
+                        </div>
+                        <div class="grid">
+                            <label for="password2">Heslo znovu</label>
+                            <input type="password" name="password2" id="password2" placeholder="Zadejte heslo znovu">
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col mt-2">
-                        <label for="city">Město</label>
-                        <input type="text" class="form-control" name="city" id="city" placeholder="město">
+                <div class="grid-cols-1">
+                    <h4 class="text-center">Bydliště</h4>
+                    <div>
+                        <div class="grid">
+                            <label for="street">Ulice</label>
+                            <input type="text" name="street" id="street" placeholder="ulice">
+                        </div>
+                        <div class="grid">
+                            <label for="housenumber">ČP</label>
+                            <input type="number" min="1" max="1000" name="housenumber" id="housenumber" placeholder="čp">
+                        </div>
+                        <div class="grid">
+                            <label for="psc">PSČ</label>
+                            <input type="number" min="10000" max="79999" name="psc" id="psc" placeholder="psč">
+                        </div>
                     </div>
-                    <div class="col mt-2">
-                        <label for="okres">Okres:</label>
-                        <input list="okresy" class="form-control" name="okres" id="okres">
-                        <datalist id="okresy">
-                            <?php
-                            $okresy = array("Benešov","Beroun","Blansko","Brno-město","Brno-venkov","Bruntál",
-                                "Břeclav","Česká Lípa","České Budějovice","Český Krumlov","Děčín","Domažlice",
-                                "Frýdek-Místek","Havlíčkův Brod","Hodonín","Hradec Králové","Cheb","Chomutov",
-                                "Chrudim","Jablonec nad Nisou","Jeseník","Jičín","Jihlava","Jindřichův Hradec",
-                                "Karlovy Vary","Karviná","Kladno","Klatovy","Kolín","Kroměříž","Kutná Hora",
-                                "Liberec","Litoměřice","Louny","Mělník","Mladá Boleslav","Most","Náchod",
-                                "Nový Jičín","Nymburk","Olomouc","Opava","Ostrava-město","Pardubice","Pelhřimov",
-                                "Písek","Plzeň-jih","Plzeň-město","Plzeň-sever","Praha-východ","Praha-západ",
-                                "Prachatice","Prostějov","Přerov","Příbram","Rakovník","Rokycany","Rychnov nad Kněžnou",
-                                "Semily","Sokolov","Strakonice","Svitavy","Šumperk","Tábor","Tachov","Teplice","Trutnov",
-                                "Třebíč","Uherské Hradiště","Ústí nad Labem","Ústí nad Orlicí","Vsetín","Vyškov","Zlín",
-                                "Znojmo","Žďár nad Sázavou");
-                            echo "<option value=''>";
-                            foreach($okresy as $okres){
-                                echo "<option value='$okres'>$okres";
-                            }
-                            ?>
-                        </datalist>
+                    <div>
+                        <div class="grid">
+                            <label for="city">Město</label>
+                            <input type="text" name="city" id="city" placeholder="město">
+                        </div>
+                        <div class="grid">
+                            <label for="okres">Okres:</label>
+                            <input list="okresy" name="okres" id="okres">
+                            <datalist id="okresy">
+                                <?php
+                                $okresy = array("Benešov","Beroun","Blansko","Brno-město","Brno-venkov","Bruntál",
+                                    "Břeclav","Česká Lípa","České Budějovice","Český Krumlov","Děčín","Domažlice",
+                                    "Frýdek-Místek","Havlíčkův Brod","Hodonín","Hradec Králové","Cheb","Chomutov",
+                                    "Chrudim","Jablonec nad Nisou","Jeseník","Jičín","Jihlava","Jindřichův Hradec",
+                                    "Karlovy Vary","Karviná","Kladno","Klatovy","Kolín","Kroměříž","Kutná Hora",
+                                    "Liberec","Litoměřice","Louny","Mělník","Mladá Boleslav","Most","Náchod",
+                                    "Nový Jičín","Nymburk","Olomouc","Opava","Ostrava-město","Pardubice","Pelhřimov",
+                                    "Písek","Plzeň-jih","Plzeň-město","Plzeň-sever","Praha-východ","Praha-západ",
+                                    "Prachatice","Prostějov","Přerov","Příbram","Rakovník","Rokycany","Rychnov nad Kněžnou",
+                                    "Semily","Sokolov","Strakonice","Svitavy","Šumperk","Tábor","Tachov","Teplice","Trutnov",
+                                    "Třebíč","Uherské Hradiště","Ústí nad Labem","Ústí nad Orlicí","Vsetín","Vyškov","Zlín",
+                                    "Znojmo","Žďár nad Sázavou");
+                                echo "<option value=''>";
+                                foreach($okresy as $okres){
+                                    echo "<option value='$okres'>$okres";
+                                }
+                                ?>
+                            </datalist>
+                        </div>
                     </div>
-                </div>
-                <div class="text-center col mt-4">
-                    <button type="submit" name="registruj" class="btn btn-primary">Registrovat</button>
+                    <div class="text-center">
+                        <button type="submit" name="registruj">Registrovat</button>
+                    </div>
                 </div>
             </form>
+
         </div>
-    </div>
-</div>
+
 <?php
 }else{
 ?>
