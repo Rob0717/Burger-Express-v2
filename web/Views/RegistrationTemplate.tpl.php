32654e46b5d6db868850e0d740b9bae69ca5809c
Robert Onder <134645209+Rob0717@users.noreply.github.com>
2024-02-21 08:25:27 +0100

    commit

--- web/Views/RegistrationTemplate.tpl.php
+++ web/Views/RegistrationTemplate.tpl.php
@@ -52,33 +52,31 @@ global $tplData;
 if(!$tplData['jePrihlasen']){
 ?>
     <div class="grid grid-cols-1 justify-center items-center h-screen font-roboto">
-        <form class="grid grid-cols-1 xl:grid-cols-2 space-x-5 justify-center items-center bg-emerald-100 rounded-2xl space-y-3 p-5 ml-10 mr-10" method="POST">
-            <div class="col-span-1 xl:col-span-2">
-                <h4 class="text-center">Údaje</h4>
-            </div>
+        <form class="grid grid-cols-1 xl:grid-cols-2 sm:space-x-5 justify-center items-center bg-emerald-100 rounded-2xl space-y-3 p-5 m-10" method="POST">
             <div class="col-span-1">
+                <h4 class="text-center">Údaje</h4>
                     <div class="grid">
                         <div class="grid">
-                            <label for="name">Jméno</label>
-                            <input type="text" name="name" id="name" placeholder="Zadejte jméno">
+                            <label for="name">Jméno:</label>
+                            <input class="bg-emerald-100" type="text" name="name" id="name" placeholder="Zadejte jméno">
                         </div>
                         <div class="grid">
-                            <label for="surname">Příjmení</label>
-                            <input type="text" name="surname" id="surname" placeholder="Zadejte příjmení">
+                            <label for="surname">Příjmení:</label>
+                            <input class="bg-emerald-100" type="text" name="surname" id="surname" placeholder="Zadejte příjmení">
                         </div>
                     </div>
                     <div class="grid">
-                        <label for="email">Emailová adresa</label>
-                        <input type="email" name="email" id="email" placeholder="Zadejte email">
+                        <label for="email">Emailová adresa:</label>
+                        <input class="bg-emerald-100" type="email" name="email" id="email" placeholder="Zadejte email">
                     </div>
                     <div>
                         <div class="grid">
-                            <label for="password1">Heslo</label>
-                            <input type="password" name="password1" id="password1" placeholder="Zadejte heslo">
+                            <label for="password1">Heslo:</label>
+                            <input class="bg-emerald-100" type="password" name="password1" id="password1" placeholder="Zadejte heslo">
                         </div>
                         <div class="grid">
-                            <label for="password2">Heslo znovu</label>
-                            <input type="password" name="password2" id="password2" placeholder="Zadejte heslo znovu">
+                            <label for="password2">Heslo znovu:</label>
+                            <input class="bg-emerald-100" type="password" name="password2" id="password2" placeholder="Zadejte heslo znovu">
                         </div>
                     </div>
             </div>
@@ -86,26 +84,26 @@ if(!$tplData['jePrihlasen']){
                 <h4 class="text-center">Bydliště</h4>
                 <div>
                     <div class="grid">
-                        <label for="street">Ulice</label>
-                        <input type="text" name="street" id="street" placeholder="ulice">
+                        <label for="street">Ulice:</label>
+                        <input class="bg-emerald-100" type="text" name="street" id="street" placeholder="ulice">
                     </div>
                     <div class="grid">
-                        <label for="housenumber">ČP</label>
-                        <input type="number" min="1" max="1000" name="housenumber" id="housenumber" placeholder="čp">
+                        <label for="housenumber">ČP:</label>
+                        <input class="bg-emerald-100" type="number" min="1" max="1000" name="housenumber" id="housenumber" placeholder="čp">
                     </div>
                     <div class="grid">
-                        <label for="psc">PSČ</label>
-                        <input type="number" min="10000" max="79999" name="psc" id="psc" placeholder="psč">
+                        <label for="psc">PSČ:</label>
+                        <input class="bg-emerald-100" type="number" min="10000" max="79999" name="psc" id="psc" placeholder="psč">
                     </div>
                 </div>
                 <div>
                     <div class="grid">
-                        <label for="city">Město</label>
-                        <input type="text" name="city" id="city" placeholder="město">
+                        <label for="city">Město:</label>
+                        <input class="bg-emerald-100" type="text" name="city" id="city" placeholder="město">
                     </div>
                     <div class="grid">
                         <label for="okres">Okres:</label>
-                        <input list="okresy" name="okres" id="okres">
+                        <input class="bg-emerald-100" list="okresy" name="okres" id="okres">
                         <datalist id="okresy">
                             <?php
                             $okresy = array("Benešov","Beroun","Blansko","Brno-město","Brno-venkov","Bruntál",
